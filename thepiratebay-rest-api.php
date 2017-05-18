c4b19c93682bec6dedf4635205125f0ed26c94bd
Nima HeydariNasab <nima.heydari79@yahoo.com>
2017-05-18 11:27:40 +0430

    Fixing RegEx and URL

--- thepiratebay-rest-api.php
+++ thepiratebay-rest-api.php
@@ -8,14 +8,14 @@ http://nimatv.ir
 
 header('Content-Type: application/json');
 if(isset($_GET['q'])) {
-  $page = file_get_contents('https://thepiratebay.life/search/'.urlencode($_GET['q']).'/0/99/0');
-  preg_match_all('/<div class="detName.*>(.*)<.*\n.*\n<a href="(.*)" title="D.*\n<font class="detDesc">Uploaded (.*), Size (.*), ULed by <a class="detDesc" href=".*" title=".*">.*<\/a><\/font>\n<\/td>\n<td align="right">(.*)<\/td>\n<td align="right">(.*)<\/td>/', $page, $links);
+  $page = file_get_contents('https://thepiratebay.org/search/'.urlencode($_GET['q']).'/0/99/0');
+  preg_match_all('/<div class="detName">.*<a href=".*" class="detLink" title=".*">(.*)<\/a>\n<\/div>\n<a href="(.*)" title="Download this torrent using magnet"><img src=".*\n.*<font class="detDesc">Uploaded (.*), Size (.*), ULed by <a class="detDesc" href=".*" title="Browse .*">.*<\/a><\/font>\n.*<\/td>\n.*<td align="right">(.*)<\/td>\n.*<td align="right">(.*)<\/td>/', $page, $links);
   $result = [];
-  if($page[0] === '<') {
-    exit('{"error":"nothing found"}');
-  }
   for($i = 0; $i<count($links[0]); $i++) {
     $result[] = ['title' => $links[1][$i], 'magnet' => $links[2][$i], 'size' => str_replace('&nbsp;', ' ', $links[4][$i]), 'date' => str_replace('&nbsp;', ' ', $links[3][$i]), 'seeders' => $links[5][$i], 'leechers' => $links[6][$i]];
+  }
+  if($result === []) {
+    exit('{"error":"nothing found"}');
   }
   exit(json_encode($result));
 }
